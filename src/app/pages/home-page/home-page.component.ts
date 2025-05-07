04582cba2cb9580fe25202800d2078ea1d12283c
linnnxy <linxyharu@gmail.com>
2025-05-07 02:44:19 -0300

    adiciona funcionalidade de criação de notas e navegação na página inicial

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { NoteStoreService } from '../../services/note-store.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
